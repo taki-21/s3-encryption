2cd62d09cf7fbcf2a5cdfd75ba3eea1b9b39acc8
taki-21 <taki.it.0208.21@gmail.com>
2022-10-30 23:33:36 +0900

    add: get object for boto3

---
+++ operation_for_boto3.py
@@ -0,0 +1,34 @@
+# 参考:
+# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3.Client.get_object
+
+import os
+
+import boto3
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(verbose=True, dotenv_path=dotenv_path)
+
+SSE_CUSTOMER_KEY_BASE64 = os.environ.get("SSE_CUSTOMER_KEY_BASE64")
+SSE_CUSTOMER_KEY_MD5 = os.environ.get("SSE_CUSTOMER_KEY_MD5")
+
+PROFILE = os.environ.get("PROFILE")
+BUCKET_NAME = os.environ.get("BUCKET_NAME")
+KEY = os.environ.get("KEY")
+session = boto3.Session(profile_name=PROFILE)
+client = session.client('s3')
+
+response = client.get_object(
+    Bucket=BUCKET_NAME,
+    Key=KEY,
+    SSECustomerAlgorithm='AES256',
+    SSECustomerKey=SSE_CUSTOMER_KEY_BASE64,
+    SSECustomerKeyMD5=SSE_CUSTOMER_KEY_MD5
+)
+
+print('response: ', response)
+
+binary = response['Body'].read()
+
+with open('./tmp/a.jpg', "wb") as f:
+    f.write(binary)
